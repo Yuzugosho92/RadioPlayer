3ebbdbf0de529f72e87972273d87c908abad603c
Yuzugosho92 <yuzukosho92@gmail.com>
2025-12-26 06:20:48 +0900

    微調整

--- RadioPlayer1/Setting.vb
+++ RadioPlayer1/Setting.vb
@@ -13,7 +13,7 @@
     Public Property Gein As Boolean
 
     '交通情報を挟むかどうか
-    Public Property Traffic As Boolean
+    Public Property Traffic As Boolean = True
 
     '交通情報の時間間隔
     Public Property TrafficInterval As Integer = 30
